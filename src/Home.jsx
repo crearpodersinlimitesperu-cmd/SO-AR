15968fbab4c7afed981010caba73d8ac7db300b0
crearpodersinlimitesperu-cmd <crearpodersinlimitesperu@gmail.com>
2026-09-17 13:27:48 -0500

    fix(home): pase de prop currentUser a HorariosEntrenamientoModal

--- src/Home.jsx
+++ src/Home.jsx
@@ -2693,7 +2693,7 @@ export default function Home() {
       )}
 
       {/* MODAL HORARIOS DE ENTRENAMIENTOS Y CÓDIGO DE VESTIMENTA */}
-      <HorariosEntrenamientoModal isOpen={showHorariosModal} onClose={() => setShowHorariosModal(false)} />
+      <HorariosEntrenamientoModal isOpen={showHorariosModal} onClose={() => setShowHorariosModal(false)} currentUser={currentUser} />
     </div>
   );
 }
